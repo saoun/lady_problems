836c913dad4daf38b81aa2b61fcc6fc07dd33985
Alejandra Jimenez <axjimenez89@gmail.com>
2016-10-08 13:23:32 -0700

    yes button works

---
+++ app/assets/javascripts/home/home.js
@@ -0,0 +1,16 @@
+console.log("home.js is working!")
+
+$(document).ready(function()
+{
+  $(".yes").on("click", function(){
+    console.log("click working")
+  $(".intro1").hide()
+  $(".intro2").show();
+    });
+
+
+})
+
+
+
+
