326f4313c6cc14e862363b98d31784c1b9eb2bf5
Jeff Ahking <jeffahking@gmail.com>
2016-10-09 11:46:19 -0400

    fix conflict

--- app/assets/javascripts/home/home.js
+++ app/assets/javascripts/home/home.js
@@ -1,27 +1,5 @@
-<<<<<<< HEAD
-console.log("home.js is working!")
 
-$(document).ready(function()
-{
-  $(".yes").on("click", function(){
-    console.log("click working")
-  $(".intro1").hide()
-  $(".intro2").show();
-    });
-  $(".no").on("click", function(){
-    console.log("no click working")
-  $(".intro1").hide()
-  $(".intro2").show();
-    });
-
-})
-
-
-
-
-=======
 $(document).ready(function(){
   console.log("hi");
       $('.carousel.carousel-slider').carousel({full_width: true});
     });
->>>>>>> b165defcf94b276b2ddfbe215eb81c68505d40d1
