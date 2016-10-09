155557f888003fd29b105c91335da5d20dbe64b6
Stefan Hartmann <stefanhartmann@gmail.com>
2016-10-09 08:30:10 -0400

    add migration to change Post[:type] to Post[:post_type] to avoid namespace collision

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161009121452) do
+ActiveRecord::Schema.define(version: 20161009122809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,7 +69,7 @@ ActiveRecord::Schema.define(version: 20161009121452) do
     t.string   "content"
     t.integer  "user_id"
     t.integer  "board_id"
-    t.string   "type"
+    t.string   "post_type"
     t.index ["board_id"], name: "index_posts_on_board_id", using: :btree
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
