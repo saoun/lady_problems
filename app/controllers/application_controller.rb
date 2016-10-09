638f67dff6a0362d2bdc1e588eecdc98120dc949
Jeff Ahking <jeffahking@gmail.com>
2016-10-09 13:12:22 -0400

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   private
   def current_user
-
+    session[:user_id] = nil;
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
