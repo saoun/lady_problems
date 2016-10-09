78cd502cb5ad465564b5cff564700ef8882f965b
Stefan Hartmann <stefanhartmann@gmail.com>
2016-10-08 21:20:56 -0400

    update routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  resources :feedbacks
-  resources :boards
-  resources :comments
-  resources :posts
-  resources :pitches
   resources :users
+
+  resources :boards do
+    resources :posts do
+      resources :comments
+    end
+  end
+
+  resources :pitches do
+    resources :feedbacks
+  end
+
 
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
